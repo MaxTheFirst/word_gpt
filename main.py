820fc6364ec5881803cc8125bd341a757af4b827
MaxTheFirst <maks.vershinin.2005@mail.ru>
2023-08-30 21:44:37 +0300

    v1.0.0

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from win32com import __gen_path__
 from gpt import ChatGPT
 from config import API_KEY
 import win32com.client as win32
+from os import walk
+from shutil import rmtree
+
+
 
 def get_selections():
-    word = win32.gencache.EnsureDispatch('Word.Application')
-    word.ActiveDocument
+    try:
+        word = win32.gencache.EnsureDispatch('Word.Application')
+        word.ActiveDocument
+    except AttributeError:
+        paths = list(walk(__gen_path__))
+        rmtree(paths[0][0] + '\\' + paths[0][1][0])
+        return get_selections()
     return word.Selection
 
 def main():
